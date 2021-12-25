a36b92d6584dd416addc940b8e040b9e540eb6a6
caplanmaor <caplanmaor@gmail.com>
2021-12-25 14:33:44 +0200

    added catalog fade transition

--- script.js
+++ script.js
@@ -40,87 +40,101 @@ let shoes = [
   },
 ];
 
-//scale animation on hover for categories
-let navCategories = document.getElementsByClassName("category");
-for (let navCategory of navCategories) {
-  navCategory.addEventListener("mouseover", function () {
-    navCategory.classList.add("transition");
-    navCategory.classList.remove("reverse");
-  });
-  navCategory.addEventListener("mouseout", function () {
-    navCategory.classList.add("reverse");
-  });
+createShoes();
+categoriesScaleAnimation();
+categoriesEventSetup();
+
+function categoriesScaleAnimation() {
+  let navCategories = document.getElementsByClassName("category");
+  for (let navCategory of navCategories) {
+    navCategory.addEventListener("mouseover", function () {
+      navCategory.classList.add("transition");
+      navCategory.classList.remove("reverse");
+    });
+    navCategory.addEventListener("mouseout", function () {
+      navCategory.classList.add("reverse");
+    });
+  }
 }
 
-//set up logo  button
-document.getElementById("logo").addEventListener("click", function () {
-  createShoes();
-});
-//set up men button
-document.getElementById("menbtn").addEventListener("click", function () {
-  createShoes("Men");
-});
-//set up wommen button
-document.getElementById("womenbtn").addEventListener("click", function () {
-  createShoes("Women");
-});
-//set up adidas button
-document.getElementById("adidasbtn").addEventListener("click", function () {
-  createShoes("Adidas");
-});
-//set up yeezy button
-document.getElementById("yeezybtn").addEventListener("click", function () {
-  createShoes("Yeezy");
-});
-//set up nike button
-document.getElementById("nikebtn").addEventListener("click", function () {
-  createShoes("Nike");
-});
-//set up jordan button
-document.getElementById("jordanbtn").addEventListener("click", function () {
-  createShoes("Jordan");
-});
+function categoriesEventSetup() {
+  //set up logo  button
+  document.getElementById("logo").addEventListener("click", function () {
+    createShoes();
+  });
+  //set up men button
+  document.getElementById("menbtn").addEventListener("click", function () {
+    createShoes("Men");
+  });
+  //set up wommen button
+  document.getElementById("womenbtn").addEventListener("click", function () {
+    createShoes("Women");
+  });
+  //set up adidas button
+  document.getElementById("adidasbtn").addEventListener("click", function () {
+    createShoes("Adidas");
+  });
+  //set up yeezy button
+  document.getElementById("yeezybtn").addEventListener("click", function () {
+    createShoes("Yeezy");
+  });
+  //set up nike button
+  document.getElementById("nikebtn").addEventListener("click", function () {
+    createShoes("Nike");
+  });
+  //set up jordan button
+  document.getElementById("jordanbtn").addEventListener("click", function () {
+    createShoes("Jordan");
+  });
+}
 
 function clearCatalog() {
   //clear previous dom elements
   let previousShoes = document.querySelectorAll(".shoecard");
   for (let previousShoe of previousShoes) {
-    previousShoe.setAttribute("style", "display: none;");
+    previousShoe.remove();
   }
+}
+
+function catalogFadeOut() {
+  catalog.classList.add("fadeout");
+  catalog.classList.remove("reverse");
 }
 
 function createShoes(filter) {
-  clearCatalog();
-
-  //go over database and create shoes
-  for (let shoe of shoes) {
-    //filter by category
-    if (
-      shoe.category[0] == filter ||
-      shoe.category[1] == filter ||
-      filter == undefined
-    ) {
-      //create shoe cards
-      let shoeCard = document.createElement("div");
-      shoeCard.classList.add("shoecard");
-      shoeCard.setAttribute("data-tilt", "");
-      shoeCard.setAttribute("data-tilt-reverse", true);
-      //create shoe name
-      let shoeName = document.createElement("p");
-      shoeName.innerText = shoe.name;
-      shoeCard.appendChild(shoeName);
-      //create shoe image
-      let shoeImg = document.createElement("img");
-      shoeImg.src = shoe.img;
-      shoeCard.appendChild(shoeImg);
-      //create price
-      let shoePrice = document.createElement("h2");
-      shoePrice.innerText = shoe.price + "$";
-      shoeCard.appendChild(shoePrice);
-      //append card to catalog
-      catalog.appendChild(shoeCard);
+  catalogFadeOut();
+  catalog.addEventListener("transitionend", function () {
+    clearCatalog();
+    //go over database and create shoes
+    for (let shoe of shoes) {
+      //filter by category
+      if (
+        shoe.category[0] == filter ||
+        shoe.category[1] == filter ||
+        filter == undefined
+      ) {
+        //create shoe cards
+        let shoeCard = document.createElement("div");
+        shoeCard.classList.add("shoecard");
+        shoeCard.setAttribute("data-tilt", "");
+        shoeCard.setAttribute("data-tilt-reverse", true);
+        //create shoe name
+        let shoeName = document.createElement("p");
+        shoeName.innerText = shoe.name;
+        shoeCard.appendChild(shoeName);
+        //create shoe image
+        let shoeImg = document.createElement("img");
+        shoeImg.src = shoe.img;
+        shoeCard.appendChild(shoeImg);
+        //create price
+        let shoePrice = document.createElement("h2");
+        shoePrice.innerText = shoe.price + "$";
+        shoeCard.appendChild(shoePrice);
+        //append card to catalog
+        catalog.appendChild(shoeCard);
+      }
+      VanillaTilt(document.querySelector(".shoecard"), {});
     }
-    VanillaTilt(document.querySelector(".shoecard"), {});
-  }
+    catalog.classList.add("reverse");
+  });
 }
-createShoes();
